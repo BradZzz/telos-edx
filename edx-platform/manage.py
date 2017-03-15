40c3048cbbc8ecb6efb306b04c51a2630307327f
Mauve3 <mauvemoonman@gmail.com>
2017-03-15 14:00:46 -0700

    fingers crossed

--- edx-platform/manage.py
+++ edx-platform/manage.py
@@ -25,58 +25,58 @@ import contracts
 def parse_args():
     """Parse edx specific arguments to manage.py"""
     parser = ArgumentParser()
-    subparsers = parser.add_subparsers(title='system', description='edX service to run')
+    subparsers = parser.add_subparsers(title="system", description="edX service to run")
 
     lms = subparsers.add_parser(
-        'lms',
-        help='Learning Management System',
+        "lms",
+        help="Learning Management System",
         add_help=False,
-        usage='%(prog)s [options] ...'
+        usage="%(prog)s [options] ..."
     )
-    lms.add_argument('-h', '--help', action='store_true', help='show this help message and exit')
+    lms.add_argument("-h", "--help", action="store_true", help="show this help message and exit")
     lms.add_argument(
-        '--settings',
+        "--settings",
         help="Which django settings module to use under lms.envs. If not provided, the DJANGO_SETTINGS_MODULE "
              "environment variable will be used if it is set, otherwise it will default to lms.envs.dev")
     lms.add_argument(
-        '--service-variant',
-        choices=['lms', 'lms-xml', 'lms-preview'],
-        default='lms',
-        help='Which service variant to run, when using the aws environment')
+        "--service-variant",
+        choices=["lms", "lms-xml", "lms-preview"],
+        default="lms",
+        help="Which service variant to run, when using the aws environment")
     lms.add_argument(
-        '--contracts',
-        action='store_true',
+        "--contracts",
+        action="store_true",
         default=False,
-        help='Turn on pycontracts for local development')
+        help="Turn on pycontracts for local development")
     lms.set_defaults(
         help_string=lms.format_help(),
-        settings_base='lms/envs',
-        default_settings='lms.envs.dev',
-        startup='lms.startup',
+        settings_base="lms/envs",
+        default_settings="lms.envs.dev",
+        startup="lms.startup",
     )
 
     cms = subparsers.add_parser(
-        'cms',
-        help='Studio',
+        "cms",
+        help="Studio",
         add_help=False,
-        usage='%(prog)s [options] ...'
+        usage="%(prog)s [options] ..."
     )
     cms.add_argument(
-        '--settings',
+        "--settings",
         help="Which django settings module to use under cms.envs. If not provided, the DJANGO_SETTINGS_MODULE "
              "environment variable will be used if it is set, otherwise it will default to cms.envs.dev")
-    cms.add_argument('-h', '--help', action='store_true', help='show this help message and exit')
+    cms.add_argument("-h", "--help", action="store_true", help="show this help message and exit")
     cms.add_argument(
-        '--contracts',
-        action='store_true',
+        "--contracts",
+        action="store_true",
         default=False,
-        help='Turn on pycontracts for local development')
+        help="Turn on pycontracts for local development")
     cms.set_defaults(
         help_string=cms.format_help(),
-        settings_base='cms/envs',
-        default_settings='cms.envs.dev',
-        service_variant='cms',
-        startup='cms.startup',
+        settings_base="cms/envs",
+        default_settings="cms.envs.dev",
+        service_variant="cms",
+        startup="cms.startup",
     )
 
     edx_args, django_args = parser.parse_known_args()
@@ -98,7 +98,7 @@ if __name__ == "__main__":
 
     os.environ.setdefault("SERVICE_VARIANT", edx_args.service_variant)
 
-    enable_contracts = os.environ.get('ENABLE_CONTRACTS', False)
+    enable_contracts = os.environ.get("ENABLE_CONTRACTS", False)
     # can override with '--contracts' argument
     if not enable_contracts and not edx_args.contracts:
         contracts.disable_all()
@@ -106,7 +106,7 @@ if __name__ == "__main__":
     if edx_args.help:
         print "Django:"
         # This will trigger django-admin.py to print out its help
-        django_args.append('--help')
+        django_args.append("--help")
 
     startup = importlib.import_module(edx_args.startup)
     startup.run()
